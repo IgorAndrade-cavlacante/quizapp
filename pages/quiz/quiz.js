44856f39c765a7edda1a5467bf9b5dd0ced849ef
Igor Andrade Cavalcante 22C <aigor0863@gmail.com>
2024-06-06 01:17:03 +0000

    adicionadas as funcoes de proxima perguntas e de finalizar o quiz

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -100,7 +100,7 @@ function montarPergunta() {
                     </label>
                 </form>
 
-                <button>Enviar</button>
+                <button>Responder</button>
             </section>
     `
 }
@@ -118,6 +118,18 @@ function guardarResposta (evento) {
 }
 
 function validarResposta() {
+    const botaoEnviar = document.querySelector(".alternativas button")
+    botaoEnviar.innerText = "Proxima"
+    botaoEnviar.removeEventListener("click", validarResposta)
+    
+    if(pergunta === 10) {
+        botaoEnviar.innerText = "Finalizar"
+        botaoEnviar.addEventListener("click", finalizar)
+    } else {
+        botaoEnviar.addEventListener("click", proximaPergunta)
+    }
+
+
     if (resposta === quiz.questions[pergunta-1].answer) {
         document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "correta")
         pontos = pontos + 1
@@ -125,15 +137,22 @@ function validarResposta() {
         document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "errada")
         document.querySelector(`label[for='${respostaCorretaId}']`).setAttribute("id", "correta")
     }
+
+    pergunta = pergunta + 1
 }
 
+function finalizar() {
+    localStorage.setItem("pontos", pontos)
 
-async function iniciar() {
-    alterarAssunto()
-await buscarPerguntas()
+    window.location.href = "../resultado/resultado.html"
+}
+
+function proximaPergunta() {
     montarPergunta()
+    adiconarEventoInputs()
+}
 
-
+function adiconarEventoInputs() {
     const inputResposta = document.querySelectorAll(".alternativas input")
     inputResposta.forEach(input => {
         input.addEventListener("click", guardarResposta)
@@ -142,6 +161,14 @@ await buscarPerguntas()
             respostaCorretaId = input.id
         }
     })
+}
+
+
+async function iniciar() {
+    alterarAssunto()
+await buscarPerguntas()
+    montarPergunta()
+    adiconarEventoInputs()
 }
 
 iniciar()
